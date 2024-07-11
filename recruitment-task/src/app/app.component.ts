ac00e888cf398e738d31b27497369779292deeff
biel4s <kamilbielawski13@gmail.com>
2024-07-11 20:08:10 +0200

    Added main content component

--- recruitment-task/src/app/app.component.ts
+++ recruitment-task/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
+import {MainContentComponent} from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
