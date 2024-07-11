0fccca68900e3480a87000a33032bc5487454d82
biel4s <kamilbielawski13@gmail.com>
2024-07-11 20:14:59 +0200

    Added third block component

--- recruitment-task/src/app/main-content/main-content.component.ts
+++ recruitment-task/src/app/main-content/main-content.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {FirstBlockComponent} from "./components/first-block/first-block.component";
 import {SecondBlockComponent} from "./components/second-block/second-block.component";
+import {ThirdBlockComponent} from "./components/third-block/third-block.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   imports: [
     FirstBlockComponent,
-    SecondBlockComponent
+    SecondBlockComponent,
+    ThirdBlockComponent
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
