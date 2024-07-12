96333196b9b94b79fd0907733d093e9f17709883
biel4s <kamilbielawski13@gmail.com>
2024-07-12 17:27:21 +0200

    Added footer component

--- recruitment-task/src/app/app.component.ts
+++ recruitment-task/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
 import {MainContentComponent} from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
